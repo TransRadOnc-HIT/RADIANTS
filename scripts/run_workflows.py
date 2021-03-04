021f8fd58f10772649ee8ddc0d61c4d45b71b4d7
Francesco Sforazzini <francesco.sforazzini@gmail.com>
2021-03-04 18:40:36 +0100

    fixed a name bug in run_workflow

--- scripts/run_workflows.py
+++ scripts/run_workflows.py
@@ -30,15 +30,15 @@ def main():
 
         print('Processing subject {}'.format(sub_id))
 
-        workflow = workflow(
+        workflow_st = workflow(
             sub_id=sub_id, input_dir=BASE_DIR, work_dir=ARGS.work_dir,
             local_sink=ARGS.local_sink, normilize_mr_rt=ARGS.normalize_to_mr_rt,
             local_project_id=ARGS.local_project_id, normilize_rtct=ARGS.normalize_to_rt_ct,
             local_basedir=ARGS.local_dir, gtv_model=ARGS.gtv_seg_model_dir,
             tumor_model=ARGS.tumor_seg_model_dir, cores=ARGS.num_cores)
 
-        wf = workflow.workflow_setup(check_dependencies=True)
-        workflow.runner(wf)
+        wf = workflow_st.workflow_setup(check_dependencies=True)
+        workflow_st.runner(wf)
 
     print('Done!')
 
